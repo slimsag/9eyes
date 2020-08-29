9e2bbea633c8b6f47d7b9e76f0d1b7aa8fd24665
Stephen Gutekanst <stephen@hexops.com>
2020-08-29 16:55:42 -0700

    wip

--- main.go
+++ main.go
@@ -3,73 +3,27 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"log"
-	"time"
-
-	"github.com/go-ble/ble"
-	"github.com/go-ble/ble/examples/lib/dev"
-	"github.com/go-ble/ble/linux/hci/cmd"
-	"github.com/pkg/errors"
-)
-
-var (
-	device = flag.String("device", "default", "implementation of ble")
-	du     = flag.Duration("du", 50000*time.Second, "scanning duration")
-	dup    = flag.Bool("dup", true, "allow duplicate reported")
-	ge     = flag.Int("ge", 60, "")
 )
 
 func main() {
 	flag.Parse()
 
-	d, err := dev.NewDevice(*device, ble.OptScanParams(cmd.LESetScanParameters{
-		LEScanType:           0x01,   // 0x00: passive, 0x01: active
-		LEScanInterval:       0,      // 0x0004 - 0x4000; N * 0.625msec
-		LEScanWindow:         0x0004, // 0x0004 - 0x4000; N * 0.625msec
-		OwnAddressType:       0x00,   // 0x00: public, 0x01: random
-		ScanningFilterPolicy: 0x00,   // 0x00: accept all, 0x01: ignore non-white-listed.
-	}))
-	if err != nil {
-		log.Fatalf("can't new device : %s", err)
+	// Tracke Tile trackers / BLE beacons.
+	t := &tracker{
+		results: make(chan trackerResult),
 	}
-	ble.SetDefaultDevice(d)
+	go func() {
+		log.Println("Tracking tile trackers...")
+		ctx := context.Background()
+		if err := t.start(ctx); err != nil {
+			log.Fatal(err)
+		}
+	}()
 
-	// Scan for specified durantion, or until interrupted by user.
-	fmt.Printf("Scanning for %s...\n", *du)
-	ctx := ble.WithSigHandler(context.WithTimeout(context.Background(), *du))
-	chkErr(ble.Scan(ctx, *dup, advHandler, nil))
-}
-
-var watching = map[string]bool{"c0:36:ee:cd:4e:88": true}
-
-func advHandler(a ble.Advertisement) {
-	addr := a.Addr().String()
-	if a.RSSI() > -30 {
-		watching[addr] = true
-	}
-	if _, ok := watching[addr]; !ok {
-		return
-	}
-	if a.Connectable() {
-		fmt.Printf("[%s] C %v:\n", addr, a.RSSI())
-	} else {
-		fmt.Printf("[%s] N %v:\n", addr, a.RSSI())
-	}
-	if len(a.LocalName()) > 0 {
-		fmt.Printf(" Name: %s\n", a.LocalName())
-	}
-}
-
-func chkErr(err error) {
-	switch errors.Cause(err) {
-	case nil:
-	case context.DeadlineExceeded:
-		fmt.Printf("done\n")
-	case context.Canceled:
-		fmt.Printf("canceled\n")
-	default:
-		log.Fatalf(err.Error())
+	for {
+		ev := <-t.results
+		_ = ev
 	}
 }
 
